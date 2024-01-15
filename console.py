f7550241d69ce032db68d0c117675a300766fb85
HachemMaadalla <138704463+HachemMaadalla@users.noreply.github.com>
2024-01-15 02:04:24 +0100

    Update console.py

--- console.py
+++ console.py
@@ -10,3 +10,31 @@ from models.city import City
 from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+
+    def emptyline(self):
+        """Do nothing upon receiving an empty line."""
+        pass
+
+    def do_quit(self, arg):
+        """Quit command to exit the program."""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF signal to exit the program."""
+        print("")
+        return True
+
+    def help_quit(self):
+        """Prints help message for quit command."""
+        print("Quit command to exit the program.")
+
+    def help_EOF(self):
+        """Prints help message for EOF command."""
+        print("EOF signal to exit the program.")
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
