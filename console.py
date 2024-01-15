9b2ca60a1a8f8739195673e59634c01584343cdb
HachemMaadalla <138704463+HachemMaadalla@users.noreply.github.com>
2024-01-15 02:06:28 +0100

    Update console.py

--- console.py
+++ console.py
@@ -45,3 +45,36 @@ class User(BaseModel):
     password = ""
     first_name = ""
     last_name = ""
+
+class State(BaseModel):
+    """Represents a State in the HBnB project."""
+    name = ""
+
+class City(BaseModel):
+    """Represents a City in the HBnB project."""
+    state_id = ""
+    name = ""
+
+class Amenity(BaseModel):
+    """Represents an Amenity in the HBnB project."""
+    name = ""
+
+class Place(BaseModel):
+    """Represents a Place in the HBnB project."""
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
+
+class Review(BaseModel):
+    """Represents a Review in the HBnB project."""
+    place_id = ""
+    user_id = ""
+    text = ""
